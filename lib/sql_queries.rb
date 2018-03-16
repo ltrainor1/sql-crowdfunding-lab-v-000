e90ea4eb772c2c97ccdbe56fa3560867a474b4bb
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-16 00:36:00 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,6 +35,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   LEFT OUTER JOIN Pledges
   ON Users.id = Pledges.user_id
   GROUP BY Users.id
+  ORDER BY Users.name
   ORDER BY SUM(Pledges.amount) ASC"
 end
 
