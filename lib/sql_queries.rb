a229209e4504d7dbaca49ade6ec9bdda61ce233a
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-16 00:03:38 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT Projects.title, SUM(Pledges.amount)
 FROM Projects
 LEFT OUTER JOIN Pledges
-ON Projects.id = Pledges.project_id GROUP BY Projects.title;"
+ON Projects.id = Pledges.project_id GROUP BY Projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
