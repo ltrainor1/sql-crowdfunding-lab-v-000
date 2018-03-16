820d5a3edaa88a825fa824ce770aec444138b765
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-16 00:25:08 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,11 +21,11 @@ ON Users.id = Pledges.user_id GROUP BY Users.name ORDER BY Users.name ASC"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT Projects.title, a = SUM(Pledges.amount) - Projects.funding_goal
+  "SELECT Projects.title, SUM(Pledges.amount) - Projects.funding_goal
   FROM Projects
   LEFT OUTER JOIN Pledges
   ON Projects.id = Pledges.project_id
-  WHERE a > 0
+  AND SUM(Pledges.amount) - Projects.funding_goal > 0
   GROUP BY Projects.title"
 end
 
