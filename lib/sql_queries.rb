1cf17029d4f667e98fec940d8bb39a54eb6ea75b
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-16 00:27:47 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT Projects.title, SUM(Pledges.amount) - Projects.funding_goal
   FROM Projects
-  LEFT OUTER JOIN Pledges
+  FULL JOIN Pledges
   ON Projects.id = Pledges.project_id
   AND SUM(Pledges.amount) - Projects.funding_goal > 0
   GROUP BY Projects.title"
